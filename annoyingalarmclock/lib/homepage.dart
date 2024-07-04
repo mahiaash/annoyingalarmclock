205d60a44287934c825121a33668a620d722bc14
mahiaash <mahiaash@buffalo.edu>
2024-07-03 22:30:41 -0400

    corrected button placement

--- annoyingalarmclock/lib/homepage.dart
+++ annoyingalarmclock/lib/homepage.dart
@@ -5,26 +5,35 @@ class HomePage extends StatefulWidget {
 const HomePage ({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomePageState createState() => _HomePageState();
   }
   
   class _HomePageState extends State<HomePage>{
     
     void _showTimePicker(){
-      showTimePicker(context: context, initialTime: TimeOfDay.now());
+      showTimePicker(context: context, initialTime: TimeOfDay.now()
+      ).then((value){
+        setState(() {
+          
+        });
+      });
     }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child:IconButton(
-          onPressed:_showTimePicker,
-          icon: Icon(Icons.add)
-         )
-      )
-
+      appBar:AppBar(),
+      body: Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){
+          _showTimePicker();
+        } ,
+        child: Icon(
+          Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
     );
   }
   } 
